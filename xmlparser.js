de18f313cbc764b55f242b854d550b3256e212a0
Fabricio Catae <fabricio.catae@microsoft.com>
2016-02-02 11:27:34 -0200

    included sax

---
+++ xmlparser.js
@@ -0,0 +1,57 @@
+/* global $ */
+"use strict";
+
+$(document).ready(function() {
+   
+   function initTeste() {
+       $('#txtXml').text('<a><b></b><b><c></c><d></d></b><b></b></a>');
+   }
+   
+   function loadTeste() {
+       $.ajax({
+           url: 'xmlsample.xml', 
+           type: 'GET',
+           dataType: 'text',
+           success: function(data) {
+              $('#txtXml').val(data);           
+           } 
+       });
+
+   }
+   
+   loadTeste();
+   
+   $('#btnParse').click(parse);
+   
+   function parse() {
+       var text = $('#txtXml').val();
+       
+       var parser = sax.parser(true);
+       
+       parser.onopentag = function(node) {
+           console.log('opentag')
+       }
+
+        parser.onerror = function (e) {
+            console.log('e');
+        };
+        parser.ontext = function (t) {
+            console.log('t');
+        };
+        parser.onopentag = function (node) {
+            console.log('node');
+        };
+        parser.onattribute = function (attr) {
+            console.log('attr');
+        };
+        parser.onend = function () {
+            console.log('onend');
+        };
+       parser.onclosetag = function (tag) {
+            console.log('onclosetag');
+        };
+       parser.write('<xml>Hello, <who name="world">world</who>!</xml>').close();
+
+   }
+   
+});
