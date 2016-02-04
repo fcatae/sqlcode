e13b73498666b501fd374d00fd5a0359e2c0c2b9
Fabricio Catae <fabricio.catae@microsoft.com>
2016-02-04 17:42:16 -0200

    mocha in the browser

---
+++ test/html/html.parser.js
@@ -0,0 +1,16 @@
+"use strict";
+
+describe('teste1', function() {
+    it('teste 1 browser', function() {
+        assert(1 == 1);
+    })
+    it('teste 1 browser', function() {
+        assert(1 == 1);
+    })
+    it('teste 1 browser', function() {
+        assert(1 == 1);
+    })
+    it('teste 1 browser', function() {
+        assert(1 == 1);
+    })
+})
