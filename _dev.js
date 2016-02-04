f7c0006766722c908466946f2295991599506921
Fabricio Catae <fabricio.catae@microsoft.com>
2016-02-04 11:07:18 -0200

    carregar XML

--- _dev.js
+++ _dev.js
@@ -2,17 +2,26 @@
 
 var assert = require('assert');
 var path = require('path');
+var fs = require('fs');
 
 describe('Parser XML', function() {
 
-    var _parser = require( path.join(process.cwd(), '/src/xmlparser') );
-
+    var _parser;
+    var _xmltext;
+    
+    before(function Load_ParserAndText() {
+        _parser = require( path.join(process.cwd(), '/src/xmlparser') );
+        _xmltext = fs.readFileSync( path.join(process.cwd(), '/test/ringbuffer.xml') );
+    });
+    
     it('Importar o parser', function() {
         assert.notEqual(_parser, null);
     });
 
-    // it('Teste2', function() {
-    // });
+    it('Carregar o XML', function() {
+        assert.notEqual(_xmltext, null);
+        assert( _xmltext.length > 0 );
+    });
 
     // it('Teste2', function() {
     // });
