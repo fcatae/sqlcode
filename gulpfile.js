b182db36f2ff4e3b69cfcb12b34982af012f29e6
Fabricio Catae <fabricio.catae@microsoft.com>
2016-02-04 09:24:17 -0200

    gulp: added mocha

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp');
+var mocha = require('gulp-mocha');
 
 var LIB = 'src/lib/';
 
 gulp.task('lib', function() {
     return gulp.src([
+        // CSS
+        'bower_components/bootstrap/dist/css/bootstrap.css',
+        
+        // JS
+        'bower_components/bootstrap/dist/js/bootstrap.min.js',
+        'bower_components/jquery/dist/jquery.min.js',
         'bower_components/requirejs/require.js',
-        'bower_components/sax-js/lib/sax.js'
+        'bower_components/sax-js/lib/sax.js',        
         ])
         .pipe(gulp.dest(LIB));
 });
+
+gulp.task('mocha', function() {
+    return gulp.src('test/test1.js')
+        .pipe(mocha({ reporter: 'spec'}));
+})
