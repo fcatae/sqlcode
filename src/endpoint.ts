2d4454ade654b825702e6cbc989c10e9a3a006e8
Fabricio Catae <fabricio.catae@microsoft.com>
2016-02-10 16:09:51 -0200

    endpoint development with BDD

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -1,119 +1,132 @@
+declare var require: any;
+declare var process: any;
+declare var module: any;
+
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
-var ErrorOutput = (function () {
-    function ErrorOutput() {
-        this._infoMessages = [];
-        this._errorMessages = [];
+
+
+class ErrorOutput {
+    _infoMessages = [];
+    _errorMessages = [];
+    
+    reportInfo(info) {
+        this._infoMessages.push(info.message);     
     }
-    ErrorOutput.prototype.reportInfo = function (info) {
-        this._infoMessages.push(info.message);
-    };
-    ErrorOutput.prototype.reportError = function (error) {
+    reportError(error) {
         this._errorMessages.push(error);
-    };
-    Object.defineProperty(ErrorOutput.prototype, "info", {
-        get: function () {
-            return this._infoMessages.join('\n');
-        },
-        enumerable: true,
-        configurable: true
-    });
-    Object.defineProperty(ErrorOutput.prototype, "errors", {
-        get: function () {
-            return this._errorMessages;
-        },
-        enumerable: true,
-        configurable: true
-    });
-    return ErrorOutput;
-})();
-var ConsoleErrorOutput = (function () {
-    function ConsoleErrorOutput() {
     }
-    ConsoleErrorOutput.prototype.reportInfo = function (info) {
-        console.log('infoMessage');
-        console.log(info);
-    };
-    ConsoleErrorOutput.prototype.reportError = function (error) {
-        console.log('errorMessage');
-        console.log(error);
-    };
-    return ConsoleErrorOutput;
-})();
+    get info() {
+        return this._infoMessages.join('\n');
+    }
+    get errors() {
+        return this._errorMessages;
+    }
+}
+
+class ConsoleErrorOutput {
+    reportInfo(info) {
+        console.log('infoMessage'); 
+        console.log(info);     }
+    reportError(error) {
+        console.log('errorMessage'); 
+        console.log(error); 
+    }
+}
 // SqlConnection Class
-var SqlConnection = (function () {
-    function SqlConnection(parameters, listener) {
+class SqlConnection {
+    
+    private _options: any;
+    private _connection: any;
+    private _listener: any;
+    
+    constructor(parameters, listener?) {
         var config = {
             userName: parameters && parameters.username,
             password: parameters && parameters.password,
             server: parameters && parameters.servername,
             options: {
                 database: parameters && parameters.database,
-                encrypt: true,
+                encrypt: true, 
                 appName: parameters && parameters.appname,
                 connectTimeout: 15000,
                 cancelTimeout: 5000,
                 requestTimeout: 0,
                 packetSize: 8000
-            }
+            },    
         };
+        
         this._options = config;
         this._listener = (listener) ? listener : new ConsoleErrorOutput();
     }
-    SqlConnection.prototype.open = function (done) {
+    
+    open(done) {
         var connection = new Connection(this._options);
         var that = this;
-        connection.on('infoMessage', function (e) {
+        
+        connection.on('infoMessage', function(e) {
             that._listener && that._listener.reportInfo(e);
         });
-        connection.on('errorMessage', function (e) {
+        connection.on('errorMessage', function(e) {
             that._listener && that._listener.reportError(e);
         });
-        connection.on('end', function (e) {
+
+        connection.on('end', function(e) {
             // TODO: handle persistent connection
-            console.log('end');
-            console.log(e);
+            console.log('end'); 
+            console.log(e); 
         });
-        connection.on('error', function (e) {
+        connection.on('error', function(e) {
             // TODO: handle persistent connection
-            console.log('error');
-            console.log(e);
+            console.log('error'); 
+            console.log(e); 
         });
-        connection.on('connect', function (err) {
+        
+        connection.on('connect', function(err) {
             var error = (err) ? new Error(err.message) : null;
             done(error, this);
         });
+
         this._connection = connection;
-        return this;
-    };
-    return SqlConnection;
-})();
+        
+        return this;        
+    }
+}
+
 function getConnection(parameters, done) {
+    
     var conn = new SqlConnection(parameters);
+
     done(parameters == null ? new Error("fail") : null);
+    
     return conn;
 }
+
 function getConnection2(parameters, done) {
+
     var config = {
         userName: (parameters && parameters.username) || process.env.SQLSERVER_USER,
         password: (parameters && parameters.password) || process.env.SQLSERVER_PWD,
         server: (parameters && parameters.servername) || process.env.SQLSERVER_SRV,
         options: {
             database: (parameters && parameters.database) || process.env.SQLSERVER_DB,
-            encrypt: true,
+            encrypt: true, 
             appName: 'SuperConn',
             //connectTimeout
             //cancelTimeout
             requestTimeout: 0,
             packetSize: 8000
-        }
+        },    
     };
+
     var connection = new Connection(config);
-    connection.on('infoMessage', function (e) {
-        console.log('infoMessage');
-        console.log(e);
+
+    connection.on('infoMessage', function(e) {
+        console.log('infoMessage'); 
+        console.log(e); 
     });
-    connection.on('errorMessage', function (e) {
+    connection.on('errorMessage', function(e) {
+        
         // errors: https://azure.microsoft.com/pt-br/documentation/articles/sql-database-develop-error-messages/
         // error 40615,14,1
         // Cannot open server 'xxxxxxx' requested by the login. 
@@ -122,37 +135,47 @@ function getConnection2(parameters, done) {
         // run sp_set_firewall_rule on the master database to create a firewall rule 
         // for this IP address or address range. 
         // It may take up to five minutes for this change to take effect.
+        
         // error message : 18456,14,1
         // Login failed for user 'fcatae'.
-        console.log('errorMessage');
-        console.log(e);
+        
+        console.log('errorMessage'); 
+        console.log(e); 
     });
-    connection.on('end', function (e) {
+
+    connection.on('end', function(e) {
         // TODO: handle persistent connection
-        console.log('end');
-        console.log(e);
+        console.log('end'); 
+        console.log(e); 
     });
-    connection.on('error', function (e) {
+    connection.on('error', function(e) {
         // TODO: handle persistent connection
-        console.log('error');
-        console.log(e);
+        console.log('error'); 
+        console.log(e); 
     });
-    connection.on('connect', function (err) {
-        console.log('connect');
+    connection.on('connect', function(err) {
+        console.log('connect'); 
+
         (err) && console.log(err.message);
         done(err && err.message, connection);
+        
     });
+
     return connection;
 }
+
 function execute(connection, sqlcmd, progress, done) {
-    var request = new Request(sqlcmd, function (err, rowCount) {
+    
+    var request = new Request(sqlcmd, function(err, rowCount) {
         console.dir(err);
-        console.log('executed: ' + rowCount);
-        done && done();
-    });
-    request.on('columnMetadata', function (c) {
+        console.log('executed: ' + rowCount )
+        done && done();        
+        });
+
+    request.on('columnMetadata', function(c) {
         //console.dir(c);
-        var a = c.map(function (elem) {
+
+        var a = c.map(function(elem) {
             var header = {
                 name: elem.colName,
                 rawLength: elem.dataLength,
@@ -161,38 +184,48 @@ function execute(connection, sqlcmd, progress, done) {
             };
             return header.name;
         });
+        
         progress && progress(a);
         // flatten
         //_.map(function(col) {})
+            
     });
-    request.on('row', function (c) {
+    request.on('row', function(c) {
         //console.dir(c);
-        var a = c.map(function (elem) {
+        
+        var a = c.map(function(elem) {
             return (elem.value instanceof Uint8Array) ? getArray(elem.value) : elem.value;
-        });
+        })
+
         progress && progress(a);
+                
         function getArray(arr) {
             var p = '0x';
-            var b = arr.reduce(function (prev, cur) {
-                var hex = cur.toString(16);
-                hex = (hex.length == 1) ? '0' + hex : hex;
-                return prev + hex;
+            var b = arr.reduce(function(prev,cur) {
+               var hex = cur.toString(16);
+               hex = (hex.length == 1) ? '0' + hex : hex;
+               
+               return prev + hex; 
             });
             return p + b;
         }
+        
     });
-    request.on('done', function (c) {
-        console.log('done');
+    request.on('done', function(c) {
+        console.log('done');    
     });
-    request.on('doneProc', function (c) {
-        console.log('doneProc');
+    request.on('doneProc', function(c) {
+        console.log('doneProc');    
     });
-    connection.execSql(request);
+    
+    connection.execSql(request);    
 }
+
+
+
 module.exports = {
     getConnection: getConnection,
     execute: execute,
     SqlConnection: SqlConnection,
     ErrorOutput: ErrorOutput
-};
-//# sourceMappingURL=endpoint.js.map
+}
--- test/devEndpoint.js
+++ test/devEndpoint.js
@@ -1,10 +1,116 @@
 var path = require('path');
+var assert = require('assert');
+
+
+describe.only('SQL Connection', function() {
+    
+    var SqlConnection;
+    var ErrorOutput;
+    var _credentials;
+    var _localCredentials;
+    var _invalidCredentials;
+    
+    before(function getEndpoint() {
+        SqlConnection = require( path.join(process.cwd(), '/src/endpoint')).SqlConnection;
+        ErrorOutput = require( path.join(process.cwd(), '/src/endpoint')).ErrorOutput;
+        _credentials = {
+            username: process.env.SQLSERVER_USER,
+            password: process.env.SQLSERVER_PWD,
+            servername: process.env.SQLSERVER_SRV,
+            database: process.env.SQLSERVER_DB
+        };
+        _localCredentials = {
+            username: 'fabteste',
+            password: process.env.SQLSERVER_PWD,
+            servername: 'localhost',
+            database: 'master'
+        }; 
+        _invalidCredentials = {
+            username: 'aa',
+            password: '',
+            servername: process.env.SQLSERVER_SRV,
+            database: process.env.SQLSERVER_DB
+        };                
+    });
+    
+    it('Create local connection', function(done) {
+        var output = new ErrorOutput();
+        var conn = new SqlConnection(_localCredentials, output);
+        conn.open(function(conn_err) {
+            var info = output.info;
+            var errors = output.errors;
+
+            assert(conn_err == null);
+            assert( errors == null || errors.length == 0);
+            
+            //Changed database context to 'database'
+            //Changed language setting to <language>
+            assert(info && info.match(/Changed/));
+            
+            done();
+        })
+    });
+    
+    it('Create remote connection', function(done) {
+        var output = new ErrorOutput();
+        var conn = new SqlConnection(_credentials, output);
+        conn.open(function(conn_err) {
+            var info = output.info;
+            var errors = output.errors;
+
+            assert(conn_err == null);
+            assert( errors == null || errors.length == 0);
+                        
+            //Changed database context to 'database'
+            //Changed language setting to <language>
+            assert(info && info.match(/Changed/));
+            
+            done();
+        })
+    });
+
+    it('Create an invalid connection', function(done) {
+        var output = new ErrorOutput();
+        var conn = new SqlConnection(_invalidCredentials, output);
+        conn.open(function(conn_err) {
+            var info = output.info;
+            var errors = output.errors;
+
+            assert(conn_err != null);
+            assert( errors == null || errors.length > 0);
+            
+            done();
+        })
+    });
+    
+
+    
+});
 
 describe('Endpoint SQL', function() {
     
     var _endpoint;
+    var _credentials;
     
-    it('import endpoint', function() {
+    before(function getEndpoint() {
         _endpoint = require( path.join(process.cwd(), '/src/endpoint') );
+        _credentials = {
+            username: process.env.SQLSERVER_USER,
+            password: process.env.SQLSERVER_PWD,
+            servername: process.env.SQLSERVER_SRV,
+            database: process.env.SQLSERVER_DB
+        };
     });
+    
+    it('get connection', function(done) {
+        _endpoint.getConnection(_credentials, done);        
+    });    
+    
+    it('handle connection error', function(done) {
+        _endpoint.getConnection(null, function(err) {
+            assert.notEqual(err, null, "open connection must fail");
+            done();
+        });        
+    });
+    
 })
