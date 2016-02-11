2319df24cd1793ec0438e5fd506ee4e4254e66c5
Fabricio Catae <fabricio.catae@microsoft.com>
2016-02-11 19:50:40 -0200

    polyfill for string.repeat

--- src/datatransform.ts
+++ src/datatransform.ts
@@ -8,18 +8,61 @@ interface ColumnItem {
     minsize: number    
 }
 
+var StringRepeat = function(thatstrings, count) {
+    'use strict';
+    if (thatstrings == null) {
+      throw new TypeError('can\'t convert ' + thatstrings + ' to object');
+    }
+    var str = '' + thatstrings;
+    count = +count;
+    if (count != count) {
+      count = 0;
+    }
+    if (count < 0) {
+      throw new RangeError('repeat count must be non-negative');
+    }
+    if (count == Infinity) {
+      throw new RangeError('repeat count must be less than infinity');
+    }
+    count = Math.floor(count);
+    if (str.length == 0 || count == 0) {
+      return '';
+    }
+    // Ensuring count is a 31-bit integer allows us to heavily optimize the
+    // main part. But anyway, most current (August 2014) browsers can't handle
+    // strings 1 << 28 chars or longer, so:
+    if (str.length * count >= 1 << 28) {
+      throw new RangeError('repeat count must not overflow maximum string size');
+    }
+    var rpt = '';
+    for (;;) {
+      if ((count & 1) == 1) {
+        rpt += str;
+      }
+      count >>>= 1;
+      if (count == 0) {
+        break;
+      }
+      str += str;
+    }
+    // Could we try:
+    // return Array(count + 1).join(this);
+    return rpt;
+  }
+};
+
 function formatText(val,minsize) {
     var text = (val != null) ? val.toString() : '';
     const SPACE = ' ';
     if(text.length > minsize) {
         return text.substr(0,minsize);
     }
-    return text + SPACE.repeat(minsize - text.length)
+    return text + StringRepeat(SPACE, minsize - text.length)
 }
 
 function formatTextSeparator(minsize) {
     const SEPARATOR = '-';
-    return  SEPARATOR.repeat(minsize)
+    return  StringRepeat(SEPARATOR, minsize)
 }
 
 function createFormat(header) {
