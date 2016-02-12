a164168eaa1a61c7e7a064dce01c3bd3e1735ebe
Fabricio Catae <fabricio.catae@microsoft.com>
2016-02-11 23:32:18 -0200

    organized mocha tests in browser (#6)

---
+++ test/html/sqlapi.js
@@ -0,0 +1,16 @@
+describe('SQL API1', function() {
+    
+    it('SQL API1', function() {
+    });
+    
+    it('teste 12s browser', function() {
+        assert(1 == 1);
+    });
+    
+    it('este 1d browser', function() {
+        assert(1 == 1);
+    })
+    it('teste 1 browser', function() {
+        assert(1 == 2);
+    })
+});
