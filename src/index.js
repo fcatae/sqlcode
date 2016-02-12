11f3b24a79c6916ba1fcb9df81a28af178f868ab
Fabricio Catae <fabricio.catae@microsoft.com>
2016-02-12 15:45:38 -0200

    working version

--- src/index.js
+++ src/index.js
@@ -37,5 +37,39 @@ $(document).ready(function() {
     beforeConnectionOpen();
     connectionOpen();    
 
+    $('#idProgress').hide();
+
+    $('#idButton').click(function() {
+        var text = $('#idTextarea').val();
+        
+        $('#idProgress').show();        
+        $('#idButton').prop('disabled', true);
+        $('#idResults').hide();
+        
+        function connectionExecute() {
+            conn.execute(text, connectionExecuteCallback);
+            //setTimeout(connectionExecuteCallback, 2000);
+        }
+        
+        function connectionExecuteCallback(err, data) {
+            $('#idProgress').hide();
+            $('#idButton').prop('disabled', false);
+            if( err == null ) {
+                $('#idAlertExecute').hide();
+                $('#idResults').text(data);
+                $('#idResults').show();                                         
+            } else {
+                $('#idAlertExecute').show();
+                $('#idResults').hide();                     
+            }            
+        } 
+        
+        connectionExecute();
+    })
+
+    $('#idAlertExecute').hide();
+    $('#idResults').hide();
+    
+
 })
 
