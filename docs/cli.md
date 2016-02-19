58025de84f646ae51e5026e0c8ea894a3e26e46f
Fabricio Catae <fabricio.catae@microsoft.com>
2016-02-19 02:17:21 -0200

    command line

--- docs/cli.md
+++ docs/cli.md
@@ -1,1 +1,10 @@
 connect server [database] [user] [password] 
+
+-> user (spawn/store credentials)
+-> server (spawn/store server info)
+
+user -> user
+user -> server 
+
+server -> server
+server -> user
--- src/cli/connect.js
+++ src/cli/connect.js
@@ -1,38 +1,104 @@
 var argv = require('minimist')(process.argv.slice(2));
 var spawn = require('child_process').spawn;
 var chalk = require('chalk');
+var prompt = require('prompt');
 
-var DEFAULT_DATABASE = 'master';
+var DEFAULT_DATABASE = '<default>';
+var mode = false; // none/server/user
 
-var server = argv._[0] || process.env.SQLSERVER_SRV;
-var database = argv._[1] || process.env.SQLSERVER_DB || DEFAULT_DATABASE;
-var username = argv._[2] || process.env.SQLSERVER_USER;
-var password = argv._[3] || process.env.SQLSERVER_PWD;
+if(process.env.SQLSERVER_SRV) {
+    process.env.SQLSERVER_USER = null;
+    process.env.SQLSERVER_PWD = null;
+}
 
-var cmd = 'cmd';
-var args = ['/k','start'];
-var options = {
-    env: getEnv(server, database),
-    stdio: 'ignore',
-    detached: true
-};
+var server = process.env.SQLSERVER_SRV || argv._[0];
+var database = process.env.SQLSERVER_DB || argv._[1];
+var username = process.env.SQLSERVER_USER || argv._[2];
+var password = process.env.SQLSERVER_PWD || argv._[3];
 
-function getEnv(server, database) {
-    // inherit environment variables
+if(process.env.SQLSERVER_USER && (!server)) {    
+    // set the password
+    process.exit(0);
+}
+
+// sintaxe: server + database
+// opt: user + pwd
+
+// connect server + database
+// store user + pwd
+
+function getUserPassword(callback) {
+
+    prompt.start();
+    prompt.message = '';
+    prompt.delimiter = '';
+
+    prompt.get([{
+        name: 'username',
+        description: 'User:',
+        required: false,
+        default: username,
+        ask: function() { return (username == null || username == '')}
+        }, {
+        name: 'password',
+        description: 'Password:',
+        hidden: true,
+        required: false,
+        replace: '*',
+        default: password,
+        ask: function() { return (password == null || password == '')}
+        }], function (err, result) {
+        
+        callback && callback(err, result);
+    });
+    
+}
+
+getUserPassword(function(err, result) {
+
+    username = result.username;
+    password = result.password;
+    
+    (server || result.username) && spawnCommand();
+    
+});
+
+function spawnCommand() {
+    var cmd = 'cmd';
+    var args = ['/k','start'];
+    var options = {
+        env: getEnv(server, database, username, password),
+        stdio: 'ignore',
+        detached: true
+    };
+    spawn(cmd, args, options);
+}
+
+function getEnv(server, database, username, password) {
+    // set process environment variables
     var env = process.env;
 
-    var header = server.toUpperCase() + '/' + database;
-
-    env.PROMPT = chalk.green.bold(header) + ' $P$G';    
-}
-
-if(server) {
+    var header;
+    var decorate;
     
-    spawn(cmd, args, options);
+    if(server) {
+        header = server.toUpperCase() + '/' + (database || DEFAULT_DATABASE) + ' ';
+        decorate = chalk.green.bold;
+    } else if(username) {
+        header = '[' + username + '] ';
+        decorate = chalk.red.bold;
+    } else {
+        decorate = function() { return '' }
+    }
+
+    env.PROMPT = decorate(header) + '$P$G';
     
-} else {
-    // server not defined: store user and password
-
+    if(server) {
+        env.SQLSERVER_SRV = server;
+        (database) && (env.SQLSERVER_DB = database);
+    }
+    if(username) {
+        env.SQLSERVER_USER = username;
+        env.SQLSERVER_PWD = password;
+    }
 }
-
-
