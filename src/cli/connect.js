22e0f263a296f01fe5a029b2b84e4da9bf063fc3
Fabricio Catae <fabricio.catae@microsoft.com>
2016-02-19 00:35:13 -0200

    cmd line support

--- src/cli/connect.js
+++ src/cli/connect.js
@@ -1,26 +1,38 @@
+var argv = require('minimist')(process.argv.slice(2));
 var spawn = require('child_process').spawn;
 var chalk = require('chalk');
+
+var DEFAULT_DATABASE = 'master';
+
+var server = argv._[0] || process.env.SQLSERVER_SRV;
+var database = argv._[1] || process.env.SQLSERVER_DB || DEFAULT_DATABASE;
+var username = argv._[2] || process.env.SQLSERVER_USER;
+var password = argv._[3] || process.env.SQLSERVER_PWD;
+
+var cmd = 'cmd';
+var args = ['/k','start'];
+var options = {
+    env: getEnv(server, database),
+    stdio: 'ignore',
+    detached: true
+};
 
 function getEnv(server, database) {
     // inherit environment variables
     var env = process.env;
 
-    var header = (server + '/' + database).toUpperCase();
+    var header = server.toUpperCase() + '/' + database;
 
     env.PROMPT = chalk.green.bold(header) + ' $P$G';    
 }
 
+if(server) {
+    
+    spawn(cmd, args, options);
+    
+} else {
+    // server not defined: store user and password
 
-var cmd = 'cmd';
-var args = ['/k','start'];
-var options = {
-    env: getEnv('myserver', 'mydb'),
-    stdio: 'ignore',
-    detached: true
-};
+}
 
-var proc = spawn(cmd, args, options);
 
-proc.on('exit', function(code) {
-    console.log('exiting...' + code)
-})
